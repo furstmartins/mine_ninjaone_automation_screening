82c24dbb4a3bf51a29a3acb700c6fc250402928f
furstmartins <furstmartins@gmail.com>
2022-11-23 09:19:05 -0300

    Showcase about movies and degree of separation.

---
+++ step4/PowershellChallenge.ps1
@@ -0,0 +1,38 @@
+﻿#NINJARMM Code Challenge
+ 
+#Please review the below code and answer the following questions to the best of your ability: 
+
+#1.) What is this code doing?
+#This script is trying to update or install the NinjaWPM in the guest OS running on a VM.
+
+#2.) What does the program assume you need to have access to in order to execute?
+#The user Ninja needs to have Admin role.
+
+#3.) What architectures can this script run on?
+#Microsoft Windows.
+
+$vm = Get-VM -Name WPM
+$IsNinjaInstalled = "Test-Path 'HKLM:\SOFTWARE\WOW6432Node\NinjaRMM LLC\NinjaWPM'"
+$Result = Invoke-VMScript -VM $vm -ScriptText $IsNinjaInstalled -GuestUser Ninja -GuestPassword helloworld
+$ExitCode = $Result.ExitCode
+if($Result.ExitCode -ne "0") 
+{
+    Write-Host $vm Critical error on Test-Path: $ExitCode -ForegroundColor Red
+    exit 1
+}
+
+$output = ($Result.ScriptOutput).Trim();
+if ($output -eq 'True') {
+    Write-Host $vm NinjaWPM was found to be installed! Returned: $Result.ScriptOutput -ForegroundColor Green
+}
+
+$isNinjaWPMDownloaded = "Test-Path 'c:\NinjaInstaller\NinjaWPM.exe'"
+$Result = Invoke-VMScript -VM $vm -ScriptText $isNinjaWPMDownloaded -GuestUser Ninja -GuestPassword helloworld
+$ExitCode = $Result.ExitCode
+if($Result.ExitCode -ne "0") 
+{
+    Write-Host $vm Critical error on Test-Path: $ExitCode -ForegroundColor Red
+    exit 1
+}
+
+exit 0
